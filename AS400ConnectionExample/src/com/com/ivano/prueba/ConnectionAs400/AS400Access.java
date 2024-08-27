7201abeacf40f30633b08d01ca1c231c5b72efa3
fabellaivan <78384446+fabellaivan@users.noreply.github.com>
2024-08-27 10:09:25 -0300

    compilando

--- AS400ConnectionExample/src/com/com/ivano/prueba/ConnectionAs400/AS400Access.java
+++ AS400ConnectionExample/src/com/com/ivano/prueba/ConnectionAs400/AS400Access.java
@@ -1,13 +1,10 @@
 package com.ivano.prueba.ConnectionAs400;
 
 import com.ibm.as400.access.AS400;
-import com.ibm.as400.access.AS400SecurityException;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 
 public class AS400Access {
-    @Contract("_, _, _ -> new")
-    public static @NotNull AS400 createAS400Connection(String systemName, String userId, @NotNull String password) throws AS400SecurityException {
+
+    public static  AS400 createAS400Connection(String systemName, String userId, String password)  {
 
         char[] passwordCharArray = password.toCharArray();
         return new AS400(systemName, userId, "suenios$3");
